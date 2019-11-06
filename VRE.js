4b00c4fd207c16089f3b2110a924d50c8e522fef
Enrique E Perez II <SpartanO57@yahoo.com>
2019-11-06 16:48:31 -0600

    Full Test system implemented
    *Implemented a test system to help test the full system spectrem, Including Sounds, Window alerts, console alerts and any other that maybe needed to be tested later.
    *Bug Fixes.
    *Added bugs to be fixed later.

--- VRE.js
+++ VRE.js
@@ -1,13 +1,3 @@
-var eEReqMet = true;
 function testFeature() {
   window.alert("It works asshole");
-};
-
-function easterEgg() {
-  var devButton = document.getElementById("testButton");
-  if (eEReqMet === true) {
-    devButton.style.display = "block";
-  } else {
-    devButton.style.display = "none";
-  }
 };
